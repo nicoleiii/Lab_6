e40d9c06923faf947979bdb6ac79da5311872354
nicoleiii <n01646918@students.humber.ca>
2024-06-13 14:34:27 -0400

    first commit

---
+++ Lab6.js
@@ -0,0 +1,32 @@
+
+// Task 1: Working with Variables
+var myNumber = 10;
+var myString = "Hello, Javascript!";
+console.log(myNumber + myString);
+
+// Task 2: Exploring Loops
+// Use a for loop to iterate from 0 to 100
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+// Using a for loop and arithemtic operations
+for (let i = 1; i <= 5; i++) {
+    console.log(3 * i);
+}
+
+// Task 3: Discovering Functions
+// Define function and return result
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Call addNumbers and print
+console.log(addNumbers(1, 2));
+
+// Define multiplyNumbers that takes two parameters and return
+function multiplyNumbers(parameter1, parameter2) {
+    return parameter1 * parameter2;
+}
+
+// Call multiplyNumbers and print result
+console.log(multiplyNumbers(100, 2));
